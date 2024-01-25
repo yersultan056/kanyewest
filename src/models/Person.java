d7ca4065508eda49a106d7df26d7c8a4dd57ffe0
yersik <230129@astanait.edu.kz>
2024-01-25 18:41:30 +0600

    Create Person class and methods

---
+++ src/models/Person.java
@@ -0,0 +1,45 @@
+package models;
+
+public abstract class Person {
+    //fields
+    private int id;
+    private String name;
+    private String surname;
+    //Helper static variable for id
+    private static int id_gen = 1;
+    //Constructors
+    public Person(){
+        id = id_gen++;
+    }
+    //Parametrized constructor
+    public Person(String name, String surname){
+        this();
+        setName(name);
+        setSurname(surname);
+    }
+    //Output of parameters (e.g. “1. John Lennon”)
+    @Override
+    public String toString() {
+        return id + '.' + name + surname;
+    }
+    //getters and setters for fields (id have only getter)
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    //(I understand the position can be Employee or Student)
+    public String getPosition(){
+        return "Student";
+    }
+}
